1e18573947bb39f7ea8e755b0cef9f95302dcc09
hanjaejung <hanjja12@naver.com>
2022-11-09 20:02:40 +0900

    쿼리메소드 테스트

--- src/main/java/com/example/bookmanager/repository/UserRepository.java
+++ src/main/java/com/example/bookmanager/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.bookmanager.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+    Users findByName(String name);
 }
